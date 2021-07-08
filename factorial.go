53a0b837f7e390a7d8430af904e66af8b5c9d203
r0l069l <ram.lodhi@walmart.com>
2021-07-09 00:19:27 +0530

    Added type Product to test importing struct

--- factorial.go
+++ factorial.go
@@ -1,16 +1,18 @@
 package factorial
 
+import "fmt"
+
 func CalculateFactorial(num int) int {
 
-	return 10
-	// num++
-	// if num == 1 || num == 0 {
-	// 	fmt.Println("Base cond matched")
-	// 	return num
+	//return 10
+	num++
+	if num == 1 || num == 0 {
+		fmt.Println("Base cond matched")
+		return num
 
-	// }
+	}
 
-	// return num * CalculateFactorial(num-1)
+	return num * CalculateFactorial(num-1)
 }
 
 type Product struct {
